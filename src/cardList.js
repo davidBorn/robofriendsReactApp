05881450eb4b198d69132c4da82a72186228a166
davidBorn <david.born25@gmail.com>
2019-05-16 18:51:18 -0600

    first commit

---
+++ src/cardList.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => {
+  
+    return (
+    <div>
+        {
+        robots.map((user, n) => {
+            return  (<Card 
+                key={robots[n].id} 
+                id = {robots[n].id} 
+                name = {robots[n].name} 
+                email = {robots[n].email}/> 
+        );
+    })
+    }
+    </div>
+
+    );
+}
+
+export default CardList;
