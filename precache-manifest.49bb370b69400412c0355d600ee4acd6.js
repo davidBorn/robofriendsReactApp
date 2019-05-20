4d23836f38148a8d56b44d9561e48a2116f543af
davidBorn <42901656+davidBorn@users.noreply.github.com>
2019-05-19 21:44:30 -0600

    Updates

---
+++ precache-manifest.49bb370b69400412c0355d600ee4acd6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "258b6a1138ed52dd568c6a6491bc7548",
+    "url": "/robofriendsReactApp/index.html"
+  },
+  {
+    "revision": "90a3bb7bbc92c1d5ad02",
+    "url": "/robofriendsReactApp/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "9f764b726cc1cc26abf5",
+    "url": "/robofriendsReactApp/static/css/main.3964ff2c.chunk.css"
+  },
+  {
+    "revision": "90a3bb7bbc92c1d5ad02",
+    "url": "/robofriendsReactApp/static/js/2.3b8b716b.chunk.js"
+  },
+  {
+    "revision": "9f764b726cc1cc26abf5",
+    "url": "/robofriendsReactApp/static/js/main.f98b78d9.chunk.js"
+  },
+  {
+    "revision": "2ee9f4c54330ba5a1c76",
+    "url": "/robofriendsReactApp/static/js/runtime~main.7dcd48f2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsReactApp/static/media/SEGA.12a3db8b.woff"
+  }
+]);
