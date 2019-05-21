ff88e50f61cec97f27733e32081a3f3804d4f8b5
davidBorn <david.born25@gmail.com>
2019-05-20 22:42:07 -0600

    changes

--- src/containers/app.js
+++ src/containers/app.js
@@ -2,42 +2,56 @@ import React, {Component} from 'react';
 import CardList from '../components/cardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import { connect } from 'react-redux';
+import ErrorBoundary from '../components/errorboundary';
 import './app.css';
+
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+    return {
+        searchfield: state.searchfield
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return{
+    onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+    }
+}
 
 class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: [],
-            searchfield: ''
+            robots: []
+            
         }
     }
 
     componentDidMount() {
+        
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(response => response.json())
         .then(users => this.setState({robots: users}));
     }
 
-    onSearchChange = (event) => {
-        this.setState({searchfield: event.target.value})
-       
-     
-
-    }
 
     render() {
-        const {robots, searchfield} = this.state;
+        const {robots } = this.state;
+        const { searchfield, onSearchChange } = this.props;
         const filteredRobots = robots.filter(robot => {
-            return robot.name.toLowerCase().includes(searchfield.toLowerCase()) });
+            return robot.name.toLowerCase().includes(searchfield.toLowerCase()) })
             return !robots.length ?
                 <h1>Loading</h1>:
             (
             <div className = 'tc'>
                 <h1 className = 'f1'>RoboFriends</h1>
-                <SearchBox searchChange = {this.onSearchChange}/>
+                <SearchBox searchChange = {onSearchChange}/>
                 <Scroll>
-                    <CardList robots = {filteredRobots}/>
+                    <ErrorBoundary>
+                        <CardList robots = {filteredRobots}/>
+                    </ErrorBoundary>
                 </Scroll>
             </div>
             );
@@ -48,4 +62,4 @@ class App extends Component {
     
 
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import './index.css';
 import App from './containers/app';
 import * as serviceWorker from './serviceWorker';
+import { searchRobots } from './reducers';
 import 'tachyons';
 
-
+const store = createStore(searchRobots);
 
 
 ReactDOM.render(
-    
-   <App/>
+   <Provider store = {store}>
+      <App  />
+   </Provider>
 
 , document.getElementById('root'));
 
